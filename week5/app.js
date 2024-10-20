6c384bcd72a3137428af4784c541cf5400e01279
himinji <minji@g.hongik.ac.kr>
2024-10-20 18:55:46 +0900

    wil commit

--- week5/app.js
+++ week5/app.js
@@ -1,23 +1,21 @@
 const root = document.getElementById("root");
 
 
-fetch('https://jsonplaceholder.typicode.com/users1/1/todos')
-
+fetch('https://jsonplaceholder.typicode.com/photos')
  .then((response) => response.json())
-
  .then((json) => render(json));
 
 
-function render(data) {
-
- data.forEach((item) => {
-
+function render(photos) {
+ data.slice(0, 10).forEach((item) => {
  const h2 = document.createElement("h2");
-
  h2.textContent = item.title;
- if (item.completed) { h2.classList.add("line"); }
+ const img = document.createElement("img");
+ img.src = item.url;
+ img.alt = item.title;
+ img.style.width = '300px';
  root.appendChild(h2);
-
+ root.appendChild(img); 
  });
 
 }
