d654f110461969743d0ac3fba889344b14781af1
himinji <minji@g.hongik.ac.kr>
2024-11-19 15:13:20 +0900

    commit

--- week10/src/Counter.js
+++ week10/src/Counter.js
@@ -1,30 +1,52 @@
 import { useState } from 'react';
 
+import CountNumber from './CountNumber';
+
 
 function Counter() {
 
  const [count, setCount] = useState(0);
 
 
- function increment() {
+ function increment10() {
 
- setCount(count+1);
-
- }
-
- const h2Style = {
-    fontSize: '30px',
-    color: 'gray'
+    setCount(count+10);
+   
     }
+   function increment() {
+  
+   setCount(count+1);
+  
+   }
+  
+   function decrement() {
+    setCount(count-1);
+   }
+   function decrement10() {
+    setCount(count-10);
+   }
+  
+   function reset() {
+    setCount(0);
+   }
+
 
  return (
 
  <div>
 
- <h2 style={h2Style}>{count}</h2>
+ <CountNumber count={count} />
 
- <button onClick={increment}>+1</button>
-
+ <button onClick={increment10}
+ style={{background:'rgb(255,0,0)',margin: '10px'}}>+10</button>
+ <button onClick={increment}
+ style={{background:'#ffff00',margin: '10px'}}>+1</button>
+ <button onClick={reset}
+ style={{background:'#00ff00',margin: '10px'}}>reset!</button>
+ <button onClick={decrement}
+ style={{background:'#00ffff',margin: '10px'}}>-1</button>
+ <button onClick={decrement10}
+ style={{background:'#0000ff',margin: '10px'}}>-10</button>
  </div>
 
  )
--- week10/src/CounterNumber.js
+++ week10/src/CounterNumber.js
@@ -1,24 +1,35 @@
 import React from 'react'
 
-
 function CountNumber(properties) {
+ function coloredNumber() {
+   
+    if (properties.count > 0) {
+      return `rgb(${properties.count}, 0, 0)`
+   }
 
- const h2Style = {
-
- fontSize: '30px',
-
- color: 'gray'
-
- }
-
-
- return (
-
- <h2 style={h2Style}>{properties.count}</h2>
-
- )
-
-}
-
-
-export default CountNumber
+    if (properties.count < 0){
+      return `rgb(0,${properties.count*-1},0)`
+   }
+   if (properties.count === 0){
+      return `rgb(0,0,0)`
+   }
+   
+    }
+    const h2Style = {
+   
+    fontSize: '30px',
+   
+    color:   `${coloredNumber()}`
+    }
+   
+    return (
+   
+    <h2 style={h2Style}>{properties.count}</h2>
+   
+    )
+   
+   }
+   
+   export default CountNumber
+   
+   
